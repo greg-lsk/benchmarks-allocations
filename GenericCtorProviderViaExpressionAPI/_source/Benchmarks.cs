2ea515c50afd47e2fe3bd4a75eb1f19ef2cb3dbf
Greg Liaskas <greg.liaskas@outlook.com>
2025-10-21 15:52:20 +0300

    update:: add benchmarks

--- GenericCtorProviderViaExpressionAPI/_source/Benchmarks.cs
+++ GenericCtorProviderViaExpressionAPI/_source/Benchmarks.cs
@@ -6,5 +6,26 @@ namespace GenericCtorProviderViaExpressionAPI;
 [MemoryDiagnoser]
 public class Benchmarks
 {
+    private int _number;
+    private string _word;
 
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _number = 1;
+        _word = "one";
+    }
+
+    [Benchmark(Baseline = true)]
+    public PairOf<int, string> SimpleCtorCall() 
+    => new(_number, _word);
+
+    [Benchmark]
+    public PairOf<int, string> StaticCtorViaExpression() 
+    => GenericCtorProvider<int, string>.StaticConstructor(_number, _word);
+
+    [Benchmark]
+    public PairOf<int, string> NonStaticCtorViaExpression() 
+    => new GenericCtorProvider<int, string>().NonStaticConstructor(_number, _word);
 }
