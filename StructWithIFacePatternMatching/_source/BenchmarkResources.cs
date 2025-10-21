705240bbd3340b35e3297d0711306f2cad75c568
Greg Liaskas <greg.liaskas@outlook.com>
2025-10-21 11:25:52 +0300

    feat:: new benchmarking method, using a check provided via an interface

--- StructWithIFacePatternMatching/_source/BenchmarkResources.cs
+++ StructWithIFacePatternMatching/_source/BenchmarkResources.cs
@@ -1,18 +1,30 @@
 namespace StructWithIFacePatternMatching;
 
+public interface IMayHasMiddleName
+{
+    public bool HasMiddleName();
+}
 
-public interface IShortName
+public interface IShortName : IMayHasMiddleName
 {
     public string FirstName { get; }
     public string LastName { get; }
 }
 
-public interface ILongName
+public interface ILongName : IMayHasMiddleName
 {
     public string FirstName { get; }
     public string MiddleName { get; }
     public string LastName { get; }
 }
 
-public readonly record struct ShortName(string FirstName, string LastName) : IShortName;
-public readonly record struct LongName(string FirstName, string MiddleName, string LastName) : ILongName;
+public readonly record struct ShortName(string FirstName, string LastName) : IShortName
+{
+    bool IMayHasMiddleName.HasMiddleName() => false;
+}
+
+
+public readonly record struct LongName(string FirstName, string MiddleName, string LastName) : ILongName
+{
+    bool IMayHasMiddleName.HasMiddleName() => true;
+}
--- StructWithIFacePatternMatching/_source/Benchmarks.cs
+++ StructWithIFacePatternMatching/_source/Benchmarks.cs
@@ -6,25 +6,34 @@ namespace StructWithIFacePatternMatching;
 [MemoryDiagnoser]
 public class Benchmarks
 {
+    private int _iterations;
     private ShortName _shortName;
-    private LongName _longName;
 
 
     [GlobalSetup]
     public void Setup()
     {
+        _iterations = 5000;
         _shortName = new("Gregg", "Allman");
-        _longName = new("Gregg", "LeNoir", "Allman");
     }
 
 
+    [Benchmark(Baseline = true)]
+    public bool FromInterfaceCheck()
+    {
+        var returnValue = true;
+        for (int i = 0; i < _iterations; ++i) returnValue = IsShortNameFromInterface(_shortName);
+        return returnValue;
+    }
+
     [Benchmark]
-    public bool PatternMatchingCheck() 
-    => PatternMatchingCheckGeneric(_shortName)
-       && PatternMatchingCheckGeneric(_longName);
+    public bool PatternMatchingCheck()    
+    {
+        var returnValue = true;
+        for (int i = 0; i < _iterations; ++i) returnValue = IsShortNameFromPatternMatching(_shortName);
+        return returnValue;
+    }
 
-
-    private static bool PatternMatchingCheckGeneric<T>(T subject) 
-        where T : struct 
-        => subject is ILongName || subject is IShortName;   
+    private static bool IsShortNameFromPatternMatching<T>(T subject) where T : struct => subject is IShortName;
+    private static bool IsShortNameFromInterface<T>(T subject) where T : struct, IMayHasMiddleName => subject.HasMiddleName();   
 }
